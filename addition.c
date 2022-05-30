219680c8c3ec2165cbd2b7a4e3dbbd4e0de7101b
Iammehran72 <106099427+Iammehran72@users.noreply.github.com>
2022-05-30 18:27:29 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+{
+      clrscr();
+
+      int a,b,sum;
+      a=2;
+      b=3;
+      sum=a+b;
+      printf("The addition is %d",sum);
+      getch();
+      return 0;
+ }
